52d502ca56c364e5c24046b7e5a7ed1d0d020ddd
ryan <rewong03@gmail.com>
2021-02-12 13:27:23 -0600

    Added local crawler, groupers, and did some refactoring

--- abyss/__init__.py
+++ abyss/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from abyss.config import Config
-from abyss.routes.crawl import crawl_api
-from abyss.utils.psql_utils import ABYSS_TABLES, create_connection, \
-    build_tables, table_exists
+from abyss.routes.orchestrate import orchestrate_api
+from abyss.utils.psql_utils import ABYSS_TABLES, read_flask_db_config, \
+    create_connection, build_tables, table_exists
 
 
 def create_app():
@@ -12,9 +12,9 @@ def create_app():
 
     @application.before_first_request
     def create_tables():
-        conn = create_connection(application)
+        conn = create_connection(read_flask_db_config(application))
         build_tables(conn, ABYSS_TABLES)
 
     # Include the API blueprint
-    application.register_blueprint(crawl_api)
+    application.register_blueprint(orchestrate_api)
     return application
---
+++ abyss/grouper/__init__.py
@@ -0,0 +1,27 @@
+from abyss.grouper.grouper import Grouper
+from abyss.grouper.ExtensionGrouper import ExtensionGrouper
+from abyss.grouper.matio_grouper import MatIOGrouper
+
+GROUPER_NAME_MAPPING = {
+    "matio": MatIOGrouper,
+    "extension": ExtensionGrouper
+}
+
+
+def get_grouper(grouper_name: str) -> Grouper:
+    """Returns a grouper by name
+
+    Parameters
+    ----------
+    grouper_name : str
+        Name of grouper to return
+
+    Returns
+    -------
+    Grouper
+        File grouper.
+    """
+    try:
+        return GROUPER_NAME_MAPPING[grouper_name]
+    except KeyError:
+        raise ValueError(f"{grouper_name} grouper does not exist")
